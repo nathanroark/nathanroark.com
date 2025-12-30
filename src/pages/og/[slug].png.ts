554ed133a3e07a6f7d2450051d218969b0de119d
Nathan Roark <nathanroark@protonmail.com>
2025-12-30 09:57:44 -0500

    remove node

--- src/pages/og/[slug].png.ts
+++ src/pages/og/[slug].png.ts
@@ -1,6 +1,5 @@
 // src/pages/og-image-only/[slug].png.ts
 export const prerender = false
-export const runtime = 'node'
 
 import { getCollection } from 'astro:content'
 import { Resvg } from '@resvg/resvg-js'
